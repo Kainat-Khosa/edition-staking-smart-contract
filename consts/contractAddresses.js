c5ed25c352fa7ce2433f785095eef42debabf508
placeparks <placeparks@gmail.com>
2023-05-13 06:16:24 -0700

    first commit

---
+++ consts/contractAddresses.js
@@ -0,0 +1,5 @@
+export const editionDropContractAddress =
+  "0xCd23Ba4E8cA45982d6b60F2365CDff9A071f6Dbb"
+export const tokenContractAddress = "0x37a2885b6Fd01123f7336990Dd7BEbBb008174c7"
+export const stakingContractAddress =
+  "0xf5413f38B7C2F41FAfE32f675b213F506Efc9CDB"
